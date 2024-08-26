6ad3cdad5138c8664cd3053450e20b66c1554f24
jasmeetsingh-1 <jasmeets9877@gmail.com>
2024-08-26 10:50:10 +0530

    contractor fix

--- src/component/page/contractors.jsx
+++ src/component/page/contractors.jsx
@@ -110,7 +110,7 @@ const ContractorsPage = () => {
           <div className='inventory-productTable-header'>
               <h3 className='cardHeader'>Suppliers</h3>
               <div className='productTable-header-buttons'>
-                  <button onClick={()=>setShowAddContractorModal(true)}>Add Product</button>
+                  <button onClick={()=>setShowAddContractorModal(true)}>Add Contractor</button>
                   <button> 
                       <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path d="M5 10H15M2.5 5H17.5M7.5 15H12.5" stroke="#5D6679" strokeWidth="1.67" strokeLinecap="round" strokeLinejoin="round"/>
